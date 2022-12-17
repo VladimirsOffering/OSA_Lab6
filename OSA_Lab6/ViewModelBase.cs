af0b50b00ed887575d1b67a79a890d174c4333ce
vladimir <qw3rty.asdd@yandex.ru>
2022-12-17 15:38:46 +0400

    add: Создан каркас проекта

---
+++ OSA_Lab6/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSA_Lab6
+{
+    class ViewModelBase
+    {
+        public class ViewModelBase : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
